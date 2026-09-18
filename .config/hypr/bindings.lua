52a13161725e2c69a5c77032da3576be0aa02c2e
Henrique <henriquecastro1198@gmail.com>
2026-09-18 01:00:55 -0300

    fix: force UTF-8 for MaisTodos tmux

--- .config/hypr/bindings.lua
+++ .config/hypr/bindings.lua
@@ -66,7 +66,7 @@ o.bind("SUPER + X", "Lab SSH",
   })
 o.bind("SUPER + C", "Mais Todos SSH",
   {
-    launch = "foot --app-id=foot-maistodos-ssh -e ssh maistodos -t 'tmux new-session -A -s main \\; set-option -t main @accent \"#cba6f7\"'",
+    launch = "foot --app-id=foot-maistodos-ssh -e ssh maistodos -t 'tmux -u new-session -A -s main \\; set-option -t main @accent \"#cba6f7\"'",
     focus =
     "^foot-maistodos-ssh$"
   })
